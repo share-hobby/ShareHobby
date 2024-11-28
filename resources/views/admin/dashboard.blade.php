a02824357f2f4ea3faf1c69eda50025f49bb5666
suzukikosei <132985856+suzukikosei@users.noreply.github.com>
2024-11-29 01:47:46 +0900

    admin_index

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,150 +1,42 @@
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>MyPage</title>
-    <!-- plugins:css -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
-     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-
-    <link rel="stylesheet" href="/vendors/feather/feather.css">
-    <link rel="stylesheet" href="/vendors/ti-icons/css/themify-icons.css">
-    <link rel="stylesheet" href="/vendors/css/vendor.bundle.base.css">
-    <script src="https://kit.fontawesome.com/YOUR_KIT_ID.js" crossorigin="anonymous"></script>
-
-    <!-- endinject -->
-    <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="/vendors/datatables.net-bs4/dataTables.bootstrap4.css">
-    <link rel="stylesheet" href="/vendors/ti-icons/css/themify-icons.css">
-    <link rel="stylesheet" type="text/css" href="/js/select.dataTables.min.css">
-    <!-- End plugin css for this page -->
-    <!-- inject:css -->
-    <link rel="stylesheet" href="/css/vertical-layout-light/style.css">
-    <!-- endinject -->
-    <link rel="shortcut icon" href="/images/favicon.png" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>生徒ホーム</title>
+    <link rel="stylesheet" href="{{ asset('css/student_home.css') }}">
 </head>
-
-
 <body>
 
-    <div class="container-scroller">
-        <!-- partial:partials/_navbar.html -->
-        @include('admin.includes.navbar')
-        <!-- partial -->
-        <div class="container-fluid page-body-wrapper">
-            <!-- partial:partials/_settings-panel.html -->
-            <div class="theme-setting-wrapper">
-                <div id="settings-trigger"><i class="ti-settings"></i></div>
-                <div id="theme-settings" class="settings-panel">
-                    <i class="settings-close ti-close"></i>
-                    <p class="settings-heading">SIDEBAR SKINS</p>
-                    <div class="sidebar-bg-options selected" id="sidebar-light-theme">
-                        <div class="img-ss rounded-circle bg-light border mr-3"></div>Light
-                    </div>
-                    <div class="sidebar-bg-options" id="sidebar-dark-theme">
-                        <div class="img-ss rounded-circle bg-dark border mr-3"></div>Dark
-                    </div>
-                    <p class="settings-heading mt-2">HEADER SKINS</p>
-                    <div class="color-tiles mx-0 px-4">
-                        <div class="tiles success"></div>
-                        <div class="tiles warning"></div>
-                        <div class="tiles danger"></div>
-                        <div class="tiles info"></div>
-                        <div class="tiles dark"></div>
-                        <div class="tiles default"></div>
-                    </div>
-                </div>
-            </div>
+    <div class="container">
+        <p>あなたは生徒です</p>
+        <form action="{{ route('admin.logout') }}" method="POST" class="logout-form">
+                @csrf
+                <button type="submit" class="btn-logout">ログアウト</button>
+        </form>
 
-            <!-- partial -->
-            <!-- partial:partials/_sidebar.html -->
-            @include('admin.includes.sidebar')
-            <!-- partial -->
-            <div class="main-panel">
-                <div class="content-wrapper">
-                    <div class="row">
-                        <div class="col-md-12 grid-margin">
-                            <div class="row">
-                                <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-                                    <h3 class="font-weight-bold">Welcome @if($LoggedAdminInfo)
-                                        <span>{{ $LoggedAdminInfo ['name'] }}</span>
-                                        <span class="badge badge-primary">{{ $LoggedAdminInfo ['role'] }}</span>
-                                        @endif
-                                    </h3>
+        <a href="{{ route('admin.chats') }}" class="btn-chat">チャット画面へ</a>
 
-                                    <h6 class="font-weight-normal mb-0">All systems are running smoothly! </h6>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- 投稿機能 -->
-                     
-                    <div>
-                        <a>投稿機能をここに追加</a>
-                    </div>
-
-
-                    <div class="col-12 grid-margin stretch-card">
-                        <div class="card">
-
-                        </div>
-                    </div>
-
-                    <!-- content-wrapper ends -->
-                    <!-- partial:partials/_footer.html -->
-                    <footer class="footer">
-                        <div class="d-sm-flex justify-content-center justify-content-sm-between">
-                            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright ©
-                                2021. Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin
-                                    template</a> from BootstrapDash. All rights reserved.</span>
-                            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made
-                                with <i class="ti-heart text-danger ml-1"></i></span>
-                        </div>
-                    </footer>
-                    <!-- partial -->
-                </div>
-                <!-- main-panel ends -->
-            </div>
-            <!-- page-body-wrapper ends -->
-        </div>
-        <!-- container-scroller -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
-        <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
-        
-
-        <!-- plugins:js -->
-        <script src="/vendors/js/vendor.bundle.base.js"></script>
-        <!-- endinject -->
-        <!-- Plugin js for this page -->
-        <script src="/vendors/chart.js/Chart.min.js"></script>
-        <script src="/vendors/datatables.net/jquery.dataTables.js"></script>
-        <script src="/vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
-        <script src="/js/dataTables.select.min.js"></script>
-
-        <!-- End plugin js for this page -->
-        <!-- inject:js -->
-        <script src="/js/off-canvas.js"></script>
-        <script src="/js/hoverable-collapse.js"></script>
-        <script src="/js/template.js"></script>
-        <script src="/js/settings.js"></script>
-        <script src="/js/todolist.js"></script>
-        <!-- endinject -->
-        <!-- Custom js for this page-->
-        <script src="/js/dashboard.js"></script>
-        <script src="/js/Chart.roundedBarCharts.js"></script>
-        <!-- End custom js for this page-->
+        <h2>投稿一覧</h2>
+        @if($posts->isEmpty())
+            <p>投稿はまだありません。</p>
+        @else
+            <ul>
+                @foreach($posts as $post)
+                    <li class="post-item">
+                        <h3>{{ $post->title }}</h3>
+                        @if($post->image)
+                            <img src="{{ asset('storage/' . $post->image) }}" alt="投稿画像">
+                        @endif
+                        <p>作成日時: {{ $post->created_at->format('Y-m-d H:i') }}</p>
+                        <a href="{{ route('posts.shows', $post->id) }}" class="btn-detail">詳細を見る</a>
+                    </li>
+                @endforeach
+            </ul>
+        @endif
+    </div>
 </body>
-
 </html>
 
 

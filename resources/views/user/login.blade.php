e7aadcbc87627564a709b55e7a3aebaff373fdd7
ryo-pixel <ryou2003ktt@gmail.com>
2024-11-29 02:41:42 +0900

    add_welcome_and_login

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -2,81 +2,73 @@
 <html lang="en">
 
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <!-- Vendor CSS -->
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+
+    <!--
     <link rel="stylesheet" href="/vendors/feather/feather.css">
     <link rel="stylesheet" href="/vendors/ti-icons/css/themify-icons.css">
     <link rel="stylesheet" href="/vendors/css/vendor.bundle.base.css">
-    <!-- Plugin CSS for this page -->
+
     <link rel="stylesheet" href="/vendors/datatables.net-bs4/dataTables.bootstrap4.css">
     <link rel="stylesheet" href="/vendors/ti-icons/css/themify-icons.css">
     <link rel="stylesheet" type="text/css" href="/usertemplate/js/select.dataTables.min.css">
-    <!-- Custom CSS -->
+
     <link rel="stylesheet" href="/css/vertical-layout-light/style.css">
     <link rel="shortcut icon" href="/images/favicon.png" />
-    <!-- Bootstrap CSS -->
+
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Toastr CSS -->
+
     <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet">
+-->
 </head>
 
 <body>
-    <div class="container-scroller">
-        <div class="container-fluid page-body-wrapper full-page-wrapper">
-            <div class="content-wrapper d-flex align-items-center auth px-0">
-                <div class="row w-100 mx-0">
-                    <div class="col-lg-4 mx-auto">
-                        <div class="auth-form-light text-left py-5 px-4 px-sm-5">
-                            @if (session('success'))
-                                <div class="alert alert-success">
-                                    {{ session('success') }}
-                                </div>
-                            @endif
-                            <h4>Hello! let's get started</h4>
-                            <h6 class="font-weight-light">Sign in to continue.</h6>
 
-                            @if ($errors->any())
-                                <div class="alert alert-danger">
-                                    <ul>
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            @endif
-
-                            <form action="{{ route('user.check') }}" method="POST">
-                                @csrf
-                                <div class="form-group">
-                                    <label for="email">Email</label>
-                                    <input type="email" class="form-control" id="email" name="email" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="password">Password</label>
-                                    <input type="password" class="form-control" id="password" name="password" required>
-                                </div>
-                                <button type="submit" class="btn btn-primary">Login</button>
-                            </form>
-
-                            <div class="text-center mt-4 font-weight-light">
-                                Don't have an account? <a href="/user/register" class="text-primary">Create</a>
-                            </div>
-                            <div class="text-center mt-2">
-                                <a class="text-primary">
-                                    <i class="fas fa-lock"></i> Forgot Password?
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- content-wrapper ends -->
+    <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+        @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
         </div>
-        <!-- page-body-wrapper ends -->
-    </div>
+        @endif
+        <h4>シェアホビへようこそ</h4>
+        <h6 class="font-weight-light">メールアドレスとパスワードの入力をお願いします。</h6>
 
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+
+        <form action="{{ route('user.check') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="email">メールアドレス</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="password">パスワード</label>
+                <input type="password" class="form-control" id="password" name="password" required>
+            </div>
+            <button type="submit" class="btn">ログイン</button>
+        </form>
+        <div class="etc">
+            <div class="mt-4 font-weight-light">
+                <a href="/user/register" class="btn new-password">新規登録の場合はこちら</a>
+            </div>
+            <div class="mt-2">
+                <a class="btn forget-password">
+                    <i class="fas fa-lock"></i> パスワードを忘れた場合はこちら
+                </a>
+            </div>
+        </div>
+    </div>
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <!-- Bootstrap JS -->
